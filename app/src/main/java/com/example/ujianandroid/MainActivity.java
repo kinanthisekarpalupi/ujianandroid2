010d8a8522f3956c71824de74eb16d9b1c5f5434
kinanthi <kinanthisekarpalupi@gmail.com>
2023-12-07 01:59:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ujianandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) EditText edNamaDepan = findViewById(R.id.edNamaDepan);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) EditText edNamaBelakang = findViewById(R.id.edNamaDepan);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button btnSimpan = findViewById(R.id.btnSimpan);
+
+        ArrayList<String> daftar_nama = new ArrayList<>();
+
+        Intent intent_list = new Intent(MainActivity.this, ListActivity.class);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String isian_nama_depan = edNamaDepan.getText().toString();
+                String isian_nama_belakang = edNamaBelakang.getText().toString();
+
+                if (isian_nama_depan.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Isian masih kosong", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Menghasilkan daftar nama dengan melewatkan bilangan genap dan menambahkan indeks
+                    for (int i = 1; i <= 20; i++) {
+                        if (i % 2 != 0) { // Skip bilangan genap
+                            String nama_lengkap = i + "." + isian_nama_depan + " " + isian_nama_belakang + " " ;
+                            daftar_nama.add(nama_lengkap);
+                        }
+                    }
+
+                    edNamaDepan.setText("");
+                    edNamaBelakang.setText("");
+                    intent_list.putStringArrayListExtra("daftar_nama", daftar_nama);
+                    startActivity(intent_list);
+                }
+            }
+        });
+    }
+}
